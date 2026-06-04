9bd1edd552c02e55c78806c83c99e2820dbf733a
Keyboard-1 <anshu045sharma@gmail.com>
2026-06-04 15:37:29 +0530

    list add activity

--- activity-1/src/App.jsx
+++ activity-1/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import Increment from './Increment'
+import ListAdd from './ListAdd'
 
 function App() {
   return (
     <div>
       <Increment />
+      <ListAdd />
     </div>
   )
 }
